a9cd7bf0cc6dcbeb9edca66a27b4c7dc3121b1df
lenny <valentin805@gmail.com>
2019-11-09 10:34:35 +0200

    Add the Map library

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-const isEven = require('is-even');
- 
-console.log(isEven(10));
+console.log('Main.js');
+const map = L.map('mapid').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	id: 'mapbox.streets',
+	accessToken: 'pk.eyJ1IjoidmFsa2EtaG9uZGEiLCJhIjoiY2sycmIxM2xqMDljNzNpbzN6d3RmYnBwOSJ9.L1NFHToyjfZ3q0O-PuE-Xw'
+}).addTo(map);
+
