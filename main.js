88d460b746cd6a02461130deedcb239e1e3c732b
lenny <valentin805@gmail.com>
2019-11-09 12:05:00 +0200

    Add click event.

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
 console.log('Main.js');
-const map = L.map('mapid').setView([42.657, 23.352], 16);
+const generatePopUpMessage = (targetName = 'Target') => `<p>${targetName}</p>`
+
+
+const mymap = L.map('mapid', {
+	// options
+	
+}).setView([42.657, 23.352], 16);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	id: 'mapbox.streets',
 	accessToken: 'pk.eyJ1IjoidmFsa2EtaG9uZGEiLCJhIjoiY2sycmIxM2xqMDljNzNpbzN6d3RmYnBwOSJ9.L1NFHToyjfZ3q0O-PuE-Xw'
-}).addTo(map);
+}).addTo(mymap);
 
+/* Go to coordinates:
+map.panTo([50, 30]);
+*/
+
+
+
+
+// get coordinates on mouse click
+const onMapClick = (e) => {
+	
+	const lat = e.latlng.lat
+	const lng = e.latlng.lng
+	console.log(lat, lng);
+	
+	
+	L.marker([lat, lng]).addTo(mymap);
+}
+mymap.on('click', onMapClick);
